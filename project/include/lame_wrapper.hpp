137974785a527600ba79c52037f6bbfcb2852340
i.domnikov <ivan.mail_list@yahoo.com>
2021-02-22 20:48:08 +0900

    Doxygen for Mp3File

--- project/include/lame_wrapper.hpp
+++ project/include/lame_wrapper.hpp
@@ -20,6 +20,7 @@ namespace folder2cpp
  * @brief Class LameWrapper to provide convinient interface for libmp3lame
  *
  * Stateless class. An instance of LameWrapper object cannot be created.
+ * Full RAII implementation
  * Has two static functions:
  * 'encode' - convert wav file into mp3
  * 'getLameVersion' - return version of libmp3lame
@@ -32,7 +33,7 @@ class LameWrapper
         /**
          * \brief Static method to encode wav file into mp3
          *
-         * Accept wav file as the first argument abd mp3 file as the second.
+         * Take wav file as the first argument abd mp3 file as the second.
          * In Inner loop it read wav, convert data into mp3 and save as mp3
          *
          * If any exception will occure during file conversion it will throw an std::runtime_error
--- project/include/mp3_file.hpp
+++ project/include/mp3_file.hpp
@@ -1,33 +1,86 @@
+/**
+ * \file cmd_options.hpp
+ * \author Domnikov Ivan
+ * \copyright © Domnikov Ivan 2021
+ * File contans Mp3File class to provide convinient interface for mp3 file
+ *
+ */
 #pragma once
 
 #include <memory>
 #include <filesystem>
 
+
+/**
+ * @brief Class Mp3File to provide convinient interface for mp3 file
+ *
+ * Full RAII implementation class for mp3 file.
+ * Class has three function:
+ * isCorrect - for checking if file was opened and ready to write
+ * getPath - return path to file
+ * write - write buffer to file
+ *
+ */
 class Mp3File
 {
-public:
-    Mp3File(const std::filesystem::path& filePath);
+    public:
 
-    bool isCorrect() const noexcept {return m_isCorrect;}
+        /**
+         * \brief Class constructor
+         *
+         * Take mp3 file path and open it. If file is opened and ready to read then isCorrect will return true
+         *
+         * \param Path to mp3 file
+         */
+        Mp3File(const std::filesystem::path& filePath);
 
-    std::filesystem::path getPath() const noexcept {return m_path;}
 
-    size_t write(unsigned char* buf, size_t length)
-    {
-        return fwrite(buf, sizeof(buf[0]), length, m_file.get());
-    }
+        /**
+         * \brief Return true if file is opened and ready to write
+         */
+        bool isCorrect() const noexcept {return m_isCorrect;}
 
-    Mp3File(const Mp3File& ) = delete;
-    Mp3File& operator=(const Mp3File& ) = delete;
 
-private:
+        /**
+         * \brief Get path to file
+         */
+        std::filesystem::path getPath() const noexcept {return m_path;}
 
-    struct FileDeleter
-    {
-        void operator()(FILE* file){fclose(file);}
-    };
 
-    std::filesystem::path m_path;
-    std::unique_ptr<FILE, FileDeleter> m_file;
-    bool m_isCorrect = false;
+        /**
+         * \brief Write buffer to file
+         *
+         * \param Pointer to buffer
+         * \param Buffer length
+         */
+        size_t write(unsigned char* buf, size_t length)
+        {
+            return fwrite(buf, sizeof(buf[0]), length, m_file.get());
+        }
+
+
+        /**
+         * \brief Copy of Mp3File cannot be created
+         */
+        Mp3File(const Mp3File& ) = delete;
+
+
+        /**
+         * \brief Mp3File cannot be copied
+         */
+        Mp3File& operator=(const Mp3File& ) = delete;
+
+    private:
+
+        /**
+         * @brief Custom deleter for file
+         */
+        struct FileDeleter
+        {
+            void operator()(FILE* file){fclose(file);}
+        };
+
+        std::filesystem::path m_path;
+        std::unique_ptr<FILE, FileDeleter> m_file;
+        bool m_isCorrect = false;
 };
